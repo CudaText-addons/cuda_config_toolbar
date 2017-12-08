99810263cfa623c2d526e01ef1582f9655f7c491
Alexey-t <support@uvviewsoft.com>
2017-12-08 14:35:36 +0300

    portable ways of save folder in cfg

--- opt.py
+++ opt.py
@@ -11,6 +11,21 @@ options = {
 fn_config = os.path.join(app.app_path(app.APP_DIR_SETTINGS), 'cuda_config_toolbar.json')
 dir_icon_sets = os.path.join(os.path.dirname(__file__), 'icons')
 dir_icons = ''
+dir_settings = app.app_path(app.APP_DIR_SETTINGS)
+macro_settings = '{op}'
+
+
+def _encode_fn(fn):
+    s = fn
+    if s.startswith(dir_settings):
+        s = macro_settings+s[len(dir_settings):]
+    return s
+
+def _decode_fn(fn):
+    s = fn
+    if s.startswith(macro_settings):
+        s = dir_settings + s[len(macro_settings):]
+    return s
 
 
 def do_load_ops():
@@ -20,13 +35,14 @@ def do_load_ops():
 
     with open(fn_config, 'r', encoding='utf8') as f:
         options = json.load(f)
-        dir_icons = options.get('dir_icons', '')
+        dir_icons = _decode_fn(options.get('dir_icons', ''))
+
 
 def do_save_ops():
     global fn_config
     global options
     global dir_icons
 
-    options['dir_icons'] = dir_icons
+    options['dir_icons'] = _encode_fn(dir_icons)
     with open(fn_config, 'w', encoding='utf8') as f:
         f.write(json.dumps(options, indent=2))
